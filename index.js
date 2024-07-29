23a499627091df879ae35644c08ad19b8e069f05
AnirudhSinh <jadejaanirudhsinh121@gmail.com>
2024-07-30 01:23:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // To parse form data
 app.use(express.static('public'));
 app.use(session({
-secret: process.env.SESSION_SECRET,
-resave: false,
-saveUninitialized: true,
-cookie: { secure: false }
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
 }));
-
 
 // Middleware to check if user is authenticated
 function isAuthenticated(req, res, next) {
-if (req.session.user) {
-next();
-} else {
-res.redirect('/login.html');
-}
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login.html');
+    }
 }
 
 // Use authMiddleware for auth routes
@@ -30,10 +29,10 @@ app.use('/api/auth', require('./api/auth'));
 
 // Use isAuthenticated middleware for secure routes
 app.get('/submit.html', isAuthenticated, (req, res) => {
-res.sendFile(path.join(__dirname, 'public', 'submit.html'));
+    res.sendFile(path.join(__dirname, 'public', 'submit.html'));
 });
 app.get('/admin.html', isAuthenticated, (req, res) => {
-res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 app.use('/api/submit', isAuthenticated, require('./api/submit'));
 app.use('/api/admin', isAuthenticated, require('./api/admin'));
@@ -41,9 +40,9 @@ app.use('/api/invite', isAuthenticated, require('./api/invite'));
 
 // Serve index.html as the home page
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
